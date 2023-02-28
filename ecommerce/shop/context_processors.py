b091370adb5ffb58c99b9612f008cbe275c6d2f0
anishoct <anishoct1985@gmail.com>
2023-02-28 11:01:42 +0530

    added

---
+++ ecommerce/shop/context_processors.py
@@ -0,0 +1,5 @@
+import shop
+from .models import Category
+def menu_links(request):
+    links=Category.objects.all()
+    return dict(links=links)
---
+++ ecommerce/shop/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+
+from . import views
+from django.conf.urls.static import static
+
+from django.conf import settings
+app_name='shop'
+urlpatterns = [
+
+    path('',views.allProdCat,name='allProdCat'),
+    path("<slug:c_slug>/",views.allProdCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetail'),
+
+]
